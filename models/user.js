d9bf207fe95545024a2fef00e864639dcd0e1897
Suyog-Sable <suyogsable3@gmail.com>
2025-06-24 18:05:21 +0530

    Signup and SignIn
    Sign up and Sign In with hashing for hashing passwords in Data Base.

--- models/user.js
+++ models/user.js
@@ -36,13 +36,28 @@ userSchema.pre("save", function (next) {
     if (!user.isModified("password")) return;
     const salt = randomBytes(16).toString();
     const hashedPassword = createHmac('sha256', salt)
-    .update(user.password)
-    .digest("hex");
+        .update(user.password)
+        .digest("hex");
 
     this.salt = salt;
     this.password = hashedPassword;
 
     next();
+});
+
+userSchema.static('matchPassword', async function (email, password) {
+    const user = await this.findOne({ email });
+    if (!user) throw new error('User Not Found!');
+
+    const salt = user.salt;
+    const hashedPassword = user.password;
+    const userProvidedHash = createHmac('sha256', salt)
+        .update(password)
+        .digest("hex");
+    if (hashedPassword !== userProvidedHash)
+        throw new Error("Incorrect Password!");
+
+    return user;
 });
 
 const User = model('user', userSchema);
